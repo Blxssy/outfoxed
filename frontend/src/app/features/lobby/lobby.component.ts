26bb59ab729ab402864ce73e0fc7d27fa8b97479
bjalyona <aly0nasm@mail.ru>
2026-04-09 01:51:05 +0300

    start lobby html draft

--- frontend/src/app/features/lobby/lobby.component.ts
+++ frontend/src/app/features/lobby/lobby.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '@fox/ui-kit/button';
 
 @Component({
-  selector: 'app-lobby',
-  imports: [],
-  templateUrl: './lobby.component.html',
-  styleUrl: './lobby.component.scss'
+    selector: 'app-lobby',
+    imports: [ButtonComponent],
+    templateUrl: './lobby.component.html',
+    styleUrl: './lobby.component.scss',
 })
-export class LobbyComponent {
-
-}
+export class LobbyComponent {}
