3833c485fd9ae055f0c1dc8ca85327cecd6f8257
bjalyona <aly0nasm@mail.ru>
2026-05-11 15:26:21 +0300

    add player cards html draft

--- frontend/src/app/features/game/game.component.ts
+++ frontend/src/app/features/game/game.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GameBoardComponent } from './game-board/game-board.component';
 import { FoxTrackComponent } from './fox-track/fox-track.component';
+import { PlayerCardsComponent } from './player-cards/player-cards.component';
 
 @Component({
     selector: 'app-game',
-    imports: [GameBoardComponent, FoxTrackComponent],
+    imports: [GameBoardComponent, FoxTrackComponent, PlayerCardsComponent],
     templateUrl: './game.component.html',
     styleUrl: './game.component.scss',
 })
